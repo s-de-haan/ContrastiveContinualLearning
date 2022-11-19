800a6ebca03b9e0d381dbae789567abffa2b6ace
Dom <dominicd7@hotmail.com>
2022-11-19 19:05:17 +0100

    Just predict even or odd

--- mlp test.py
+++ mlp test.py
@@ -46,7 +46,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = models.SupervisedContrastiveLearner()
 
 # CL Benchmark Creation
-split_mnist = SplitMNIST(n_experiences=5)
+split_mnist = SplitMNIST(n_experiences=5, shuffle=False)
 train_stream = split_mnist.train_stream
 test_stream = split_mnist.test_stream
 
@@ -54,20 +54,36 @@ test_stream = split_mnist.test_stream
 optimizer = SGD(model.parameters(), lr=0.01, momentum=0.9)
 classifier_optimizer = SGD(model.parameters(), lr=0.01, momentum=0.9)
 criterion = losses.SupConLoss()
-
-# Continual learning strategy
-cl_strategy = Naive(model, optimizer, criterion, train_mb_size=128, train_epochs=10, eval_mb_size=128, device=device, plugins=[SupConFilterPlugin()])
-classifier_strategy = Naive(model.classifier, classifier_optimizer, CrossEntropyLoss(), train_mb_size=128, train_epochs=2, eval_mb_size=128, device=device)
+classes_order = train_stream.benchmark.classes_order
 
 # train and test loop over the stream of experiences
 results = []
-for train_exp in train_stream:
+
+class EvenOddCrossEntropy(CrossEntropyLoss):
+	def __init__(self, class_order, **kwargs) -> None:
+		self.class_order = class_order
+		super(EvenOddCrossEntropy, self).__init__(**kwargs)
+
+	def forward(self, input, target):
+		target %= 2
+		return super(EvenOddCrossEntropy, self).forward(input, target)
+
+# Continual learning strategy
+cl_strategy = Naive(model, optimizer, criterion, train_mb_size=128, train_epochs=1, eval_mb_size=128, device=device, plugins=[SupConFilterPlugin()])
+classifier_strategy = Naive(model.classifier, classifier_optimizer, EvenOddCrossEntropy(classes_order), train_mb_size=128, train_epochs=1, eval_mb_size=128, device=device)
+
+for task_num, train_exp in enumerate(train_stream):
+	# train_exp.dataset.targets = (torch.tensor(train_exp.dataset.targets)==classes_order[2*task_num+1]).long()
+	train_exp.dataset.targets = train_exp.dataset.targets % 2
 	cl_strategy.train(train_exp)
 	train_exp.dataset.transform = lambda x: model.encoder(x)
-	test_stream.dataset.transform = lambda x: model.encoder(x)
 	classifier_strategy.train(train_exp)
 
-	results.append(classifier_strategy.eval(test_stream))
+	for test_exp in test_stream[:task_num+1]:
+		# test_exp.dataset.targets = (torch.tensor(test_exp.dataset.targets)==classes_order[2*task_num+1]).long()
+		test_exp.dataset.targets = test_exp.dataset.targets % 2
+		test_exp.dataset.transform = lambda x: model.encoder(x)
+		results.append(classifier_strategy.eval(test_exp))
 
 # print(results)
 print([res['Top1_Acc_Stream/eval_phase/test_stream/Task000'] for res in results])
